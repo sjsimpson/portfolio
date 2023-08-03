aaa41d23a333464a5bffcae7512ab3588ef68981
sjsimpson <spencerjsimpson@gmail.com>
2023-08-03 15:00:18 -0600

    update projects page to be mobile friendly

--- src/components/Projects/ProjectItem/ProjectItem.tsx
+++ src/components/Projects/ProjectItem/ProjectItem.tsx
@@ -11,31 +11,29 @@ const ProjectItem = (props: ProjectItemProps) => {
           <div className="project-image-container">
             <img className="project-image" src={props.image} />
           </div>
-          <div className="project-text-container">
-            <div className="project-text">
-              <div className="project-description">{props.description}</div>
-              <div className="project-links">
-                <div className="link-list">
-                  {props.githubLink && (
-                    <a href={props.githubLink} target="_blank">
-                      <div className="svg-icon-container">
-                        <span className={'svg-icon github'} />
-                      </div>
-                    </a>
-                  )}
-                  {props.websiteLink && (
-                    <a href={props.websiteLink} target="_blank">
-                      <div className="material-symbols-outlined">public</div>
-                    </a>
-                  )}{' '}
-                  {props.npmLink && (
-                    <a href={props.npmLink} target="_blank">
-                      <div className="svg-icon-container">
-                        <span className={'svg-icon npm'} />
-                      </div>
-                    </a>
-                  )}
-                </div>
+          <div className="project-text">
+            <div className="project-description">{props.description}</div>
+            <div className="project-links">
+              <div className="link-list">
+                {props.githubLink && (
+                  <a href={props.githubLink} target="_blank">
+                    <div className="svg-icon-container">
+                      <span className={'svg-icon github'} />
+                    </div>
+                  </a>
+                )}
+                {props.websiteLink && (
+                  <a href={props.websiteLink} target="_blank">
+                    <div className="material-symbols-outlined">public</div>
+                  </a>
+                )}{' '}
+                {props.npmLink && (
+                  <a href={props.npmLink} target="_blank">
+                    <div className="svg-icon-container">
+                      <span className={'svg-icon npm'} />
+                    </div>
+                  </a>
+                )}
               </div>
             </div>
           </div>
